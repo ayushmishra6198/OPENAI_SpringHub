affc548abbfa25572aa6999777907f32a71f9739
ayushmishra6198 <ayushmishra6198@gmail.com>
2024-10-02 22:45:21 +0530

    pom

---
+++ OpenAI_SpringHub/src/main/java/com/openaispringhub/OpenAI_SpringHub/OpenAiSpringHubApplication.java
@@ -0,0 +1,13 @@
+package com.openaispringhub.OpenAI_SpringHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiSpringHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiSpringHubApplication.class, args);
+	}
+
+}
